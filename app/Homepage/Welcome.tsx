bab310b21d6b1dacea804eb5e72c060703050c0b
mungaben <kamaumungai3742@gmail.com>
2024-01-12 13:12:30 +0300

    fix :: env threat

--- app/Homepage/Welcome.tsx
+++ app/Homepage/Welcome.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Welcome = () => {
     return (
-        <div>
-            <h2>
+        <div className=' gap-5 my-2 items-center flex flex-col'>
+            <h2 className=' text-center subpixel-antialiased font-semibold md:text-4xl text-lg text-gray-100'>
                 Welcome to
             </h2>
-            <h1 className=' text-themeColor'>
+            <h1 className={ ` text-center subpixel-antialiased md:text-8xl text-xl font-bold text-yellow-400`}>
                 Benkiko DAO
             </h1>
         </div>
