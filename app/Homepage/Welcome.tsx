8e4ee06758f951752cb8f752ab62679510919a91
mungaben <kamaumungai3742@gmail.com>
2024-01-15 10:08:03 +0300

    style :: home page

--- app/Homepage/Welcome.tsx
+++ app/Homepage/Welcome.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
-
 const Welcome = () => {
     return (
-        <div className=' gap-5 my-2 items-center   w-full justify-center place-items-center  flex flex-col'>
+        <div className=' gap-5 my-2 items-center  bg-yellow-600/80  w-full justify-center place-items-center  flex flex-col'>
             <h2 className=' text-center subpixel-antialiased font-semibold md:text-4xl text-lg text-gray-100'>
                 Welcome to
             </h2>
-            <h1 className={ ` text-center subpixel-antialiased md:text-8xl text-xl font-bold text-yellow-400`}>
+            <h1 className={` text-center subpixel-antialiased md:text-8xl text-xl font-bold text-yellow-400`}>
                 Benkiko DAO
             </h1>
         </div>
     )
 }
-
 export default Welcome
--- app/NavBar/Components/Logo.tsx
+++ app/NavBar/Components/Logo.tsx
@@ -4,18 +4,15 @@ import Image from 'next/image'
 const Logo = () => {
   return (
     <div className=' ml-5' >
-        <Image
+      <Image
         src={'/Images/Benkikologo.jpg'}
         alt="Benkiko Logo"
         width={50}
         height={50}
         priority
         className=' rounded-full border-2 border-white '
-        />
-
-       
+      />
     </div>
   )
 }
-
 export default Logo
